87b9ec035c7f7a77a99dd756ddfc92c158c1c5b0
unknown <yasinarafat9612@gmail.com>
2024-05-04 22:36:32 +0600

    category 2 cards done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Category from './components/Category/Category';
+import Category2 from './components/Category/Category2';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar></Navbar>
       <Hero></Hero>
       <Category></Category>
+      <Category2></Category2>
     </div>
   );
 };
--- src/components/Category/Category2.jsx
+++ src/components/Category/Category2.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import Image1 from '../../assets/category/earphone.png'
-import Image2 from '../../assets/category/watch.png'
-import Image3 from '../../assets/category/macbook.png'
+import Image1 from '../../assets/category/gaming.png'
+import Image2 from '../../assets/category/speaker.png'
+import Image3 from '../../assets/category/vr.png'
 import Button from '../Shared/Button';
 const Category = () => {
     return (
         <div className='py-8'>
             <div className='container'>
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
-                    {/* Frist col */}
-                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'>
+                   {/* Frist col */}
+                   <div className='col-span-2  py-10 pl-5 bg-gradient-to-br from-gray-500/90 to-gray-100 text-white rounded-3xl relative h-[320px] flex items-end'>
+                        <div>
+                            <div className='mb-4'>
+                                <p className='mb-[2px] text-gray-800 font-semibold'>Enjoy</p>
+                                <p className='text-2xl font-semibold mb-[2px]'>With</p>
+                                <p className='text-4xl xl:text-5xl font-bold opacity-50 mb-2'>Play Station</p>
+                                <Button
+                                    text={"Browse"}
+                                    bgColor={"bg-primary"}
+                                    textColor={"text-white"} >
+                                </Button>
+                            </div>
+                        </div>
+                        <img src={Image1} alt=""
+                            className='w-[320px] absolute top-1/2 -translate-y-1/2 -right-0' />
+                    </div>
+                    {/* Second col */}
+                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-brandGreen/70 text-white rounded-3xl relative h-[320px] flex items-end'>
                         <div>
                             <div className='mb-4'>
                                 <p className='mb-[2px] text-gray-400'>Enjoy</p>
@@ -22,11 +39,11 @@ const Category = () => {
                                 </Button>
                             </div>
                         </div>
-                        <img src={Image1} alt=""
+                        <img src={Image2} alt=""
                             className='w-[320px] absolute bottom-0' />
                     </div>
-                    {/* Second col */}
-                    <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+                    {/* Third col */}
+                    <div className='py-10 pl-5 bg-gradient-to-br from-brandBlue/20 to-brandBlue/90 text-white rounded-3xl relative h-[320px] flex items-end'>
                         <div>
                             <div className='mb-4'>
                                 <p className='mb-[2px] text-gray-400'>Enjoy</p>
@@ -39,26 +56,10 @@ const Category = () => {
                                 </Button>
                             </div>
                         </div>
-                        <img src={Image2} alt=""
-                            className='w-[320px] absolute right-[-40px]  top-[10px]' />
-                    </div>
-                    {/* Third col */}
-                    <div className='col-span-2  py-10 pl-5 bg-gradient-to-br from-black/90 to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
-                        <div>
-                            <div className='mb-4'>
-                                <p className='mb-[2px] text-gray-400'>Enjoy</p>
-                                <p className='text-2xl font-semibold mb-[2px]'>With</p>
-                                <p className='text-4xl xl:text-5xl font-bold opacity-50 mb-2'>Laptop</p>
-                                <Button
-                                    text={"Browse"}
-                                    bgColor={"bg-primary"}
-                                    textColor={"text-white"} >
-                                </Button>
-                            </div>
-                        </div>
                         <img src={Image3} alt=""
-                            className='w-[320px] absolute top-1/2 -translate-y-1/2 -right-0' />
+                            className='w-[200px] absolute right-[0px]  top-[10px]' />
                     </div>
+                    
 
                 </div>
             </div>
