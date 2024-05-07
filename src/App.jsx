1fca46d2a87f5bc81ef303832a7fc077ac83cf43
unknown <yasinarafat9612@gmail.com>
2024-05-07 23:20:54 +0600

    banner of headphone winter sell Fine Smile done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import Hero from './components/Hero/Hero';
 import Category from './components/Category/Category';
 import Category2 from './components/Category/Category2';
 import Service from './components/Services/Service';
+import Banner from './components/Banner/Banner';
+import headphone from './assets/hero/headphone.png';
+
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "!0 Jan to 28 Jan",
+  image: headphone ,
+  title2: "Air Solo Base",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt vitae molestias voluptate aliquid tenetur? Esse, quam fugit ipsa, nemo magni minus illo hic quod doloribus soluta excepturi fugiat eum delectus!",
+  bgColor: "#f42c37",
+};
+
 
 const App = () => {
   return (
@@ -13,6 +27,7 @@ const App = () => {
       <Category></Category>
       <Category2></Category2>
       <Service></Service>
+      <Banner data={BannerData}></Banner>
     </div>
   );
 };
