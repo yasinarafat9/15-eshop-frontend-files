c50648b4ec7e6ecbb2a0549428f2cad0766af0e2
unknown <yasinarafat9612@gmail.com>
2024-05-08 23:34:31 +0600

    Green watch banner 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,28 @@ import Category2 from './components/Category/Category2';
 import Service from './components/Services/Service';
 import Banner from './components/Banner/Banner';
 import headphone from './assets/hero/headphone.png';
+import smartWatch2 from './assets/category/smartwatch2-removebg-preview.png'
 import Products from './components/Products/Products';
 
 const BannerData = {
   discount: "30% OFF",
   title: "Fine Smile",
   date: "!0 Jan to 28 Jan",
-  image: headphone ,
+  image: headphone,
   title2: "Air Solo Base",
   title3: "Winter Sale",
   title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt vitae molestias voluptate aliquid tenetur? Esse, quam fugit ipsa, nemo magni minus illo hic quod doloribus soluta excepturi fugiat eum delectus!",
   bgColor: "#f42c37",
+};
+const BannerData2 = {
+  discount: "30% OFF",
+  title: "Happy Hours",
+  date: "!4 Jan to 28 Jan",
+  image: smartWatch2,
+  title2: "Smart Solo",
+  title3: "Winter Sale",
+  title4: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt vitae molestias voluptate aliquid tenetur? Esse, quam fugit ipsa, nemo magni minus illo hic quod doloribus soluta excepturi fugiat eum delectus!",
+  bgColor: "#2dcc6f",
 };
 
 
@@ -30,6 +41,7 @@ const App = () => {
       <Service></Service>
       <Banner data={BannerData}></Banner>
       <Products></Products>
+      <Banner data={BannerData2}></Banner>
     </div>
   );
 };
